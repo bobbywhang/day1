d7b5331a02f77fb847379f11a7a80c3c1f2bf871
bobbywhang <robertwhang@gmail.com>
2014-10-27 17:16:34 -0700

    added randomizer and text field with enter key and

---
+++ js/random.js
@@ -0,0 +1,23 @@
+
+$(function(){
+
+	$("input").on("keyup", function(e){
+		if (e.keyCode === 13) {
+			var $this = $(this);
+			var newItem = $this.val();	
+			if (newItem !== "") {
+				$("ul").append($("<li>").text(newItem));
+				$(this).val('');		
+			}
+		}	
+	});
+
+	$("button#randomize").on("click", function(){
+	var itemList = $("li");
+	var randomIndex = Math.floor(Math.random() * (itemList.length - 1));
+	var randomItem = itemList [randomIndex];
+	$("div#selected-random").text($(randomItem).text());
+	});
+
+});
+	
